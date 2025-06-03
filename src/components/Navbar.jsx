eda3ba4024bcdc79e03762c8c3afd9087840c303
Atenas Arita <150080590+atenasarita@users.noreply.github.com>
2025-06-03 11:41:33 -0600

    Update Navbar.jsx
    Changes to include name instead of it being a parameter in other pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axiosInstance from "../utils/axiosInstance";
 import "./Navbar.css";
 
-export default function Navbar({ username }) {
+export default function Navbar() {
   const navigate = useNavigate();
+  const [loggedUser, setUser] = useState("");
+
+  useEffect(() => {
+    const fetchUserAndRequests = async () => {
+      try {
+        const res = await axiosInstance.get("/dashboard");
+        const loggedUser = res.data.user;
+        setUser(loggedUser);
+
+      } catch (err) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+    };
+
+    fetchUserAndRequests();
+  }, [navigate]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -12,11 +30,23 @@ export default function Navbar({ username }) {
 
   return (
     <nav className="navbar">
-      <img src="/assets/kia-logo-white.png" alt="KIA" className="navbar-logo" />
-      <div className="navbar-user">
-        <span>Hola, {username}</span>
+      <div className="navbar-logo">
+        <img src="/assets/kia-logo-white.png" alt="KIA" style={{height: "40px"}} />
+        <p>Hello, {loggedUser.name} {loggedUser.surname}</p>
+      </div>
+
+      <div className="navbar-routes">
+        <div className="pages"> 
+          <Link to="/waste-registry">Log Waste</Link>
+          <Link to="/waste-history">Waste History</Link>
+          <Link to="/waste-dashboard">Dashboards</Link>
+          <Link to="/pending-requests">Pending Users</Link>
+
+        </div>
+        
         <button className="navbar-logout" onClick={handleLogout}>Cerrar sesión</button>
       </div>
+      
     </nav>
   );
 }
